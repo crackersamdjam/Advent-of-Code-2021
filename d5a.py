816349342ae59460995a96236fc6d56d04f468b9
crackersamdjam <45298690+crackersamdjam@users.noreply.github.com>
2021-12-05 14:26:25 -0500

    Add day 5

---
+++ d5a.py
@@ -0,0 +1,20 @@
+import sys
+data = sys.stdin.read().strip().split('\n')
+
+a = [[0 for i in range(1000)] for j in range(1000)]
+
+def d(x):
+	return x//abs(x) if x else 0
+
+for s in data:
+	s = list(map(int, s.replace(' -> ', ',').split(',')))
+	dx, dy = d(s[2]-s[0]), d(s[3]-s[1])
+	if dx and dy:
+		continue
+	while s[0] != s[2] or s[1] != s[3]:
+		a[s[0]][s[1]] += 1
+		s[0] += dx
+		s[1] += dy
+	a[s[2]][s[3]] += 1
+
+print(sum(sum(x > 1 for x in row) for row in a))
