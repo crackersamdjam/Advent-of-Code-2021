6e631138c62cb2e6a3eb132dad692738b897bb1b
crackersamdjam <45298690+crackersamdjam@users.noreply.github.com>
2021-12-06 14:50:44 -0500

    Add day 6

---
+++ d6a.py
@@ -0,0 +1,15 @@
+import sys
+data = sys.stdin.read().strip().split('\n')
+
+a = map(int, data[0].split(','))
+arr = [0 for i in range(9)]
+for x in a:
+	arr[x] += 1
+
+for i in range(80):
+	# print(i, sum(arr), arr)
+	x = arr.pop(0)
+	arr.append(x)
+	arr[6] += x
+
+print(sum(arr))
